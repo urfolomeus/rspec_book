3408e1c5a2a3ac217de8d0c38b7d6af241d0dcfa
urfolomeus <urfolomeus@gmail.com>
2009-11-11 18:22:06 +0000

    finished first half of first iteration of codebreaker

---
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -0,0 +1,13 @@
+Given /^I am not yet playing$/ do
+  
+end
+
+When /^I start a new game$/ do
+  @messenger = StringIO.new
+  game = Codebreaker::Game.new(@messenger)
+  game.start
+end
+
+Then /^I should see "([^\"]*)"$/ do |message|
+  @messenger.string.split("\n").should include(message)
+end
