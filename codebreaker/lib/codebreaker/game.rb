58a5125c23a5f3d1459c800d51c0f3dc22bdc9ec
urfolomeus <urfolomeus@gmail.com>
2009-11-12 15:23:02 +0000

    second iteration

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -4,9 +4,22 @@ module Codebreaker
       @messenger = messenger
     end
     
-    def start
+    def start(code)
+      @code = code
       @messenger.puts "Welcome to Codebreaker!"
       @messenger.puts "Enter guess:"
+    end
+    
+    def guess(guess)
+      result = [nil, nil, nil, nil]
+      guess.each_with_index do |peg, index|
+        if @code[index] == peg
+          result[index] = "b"
+        elsif @code.include?(peg)
+          result[@code.index(peg)] ||= "w"
+        end
+      end
+      @messenger.puts result.compact.sort.join
     end
   end
 end
